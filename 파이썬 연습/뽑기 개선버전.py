98ec6d2f45c71e36a775db0393f04e04ab56c854
mingtian-chan <sniwoo98@naver.com>
2021-12-17 17:58:02 +0900

    21.12.17 UTF-8 과 ASCII encoding 과 decoding 을 확인해보았다.

--- 파이썬 연습/뽑기 개선버전.py
+++ 파이썬 연습/뽑기 개선버전.py
@@ -1,10 +1,24 @@
 import random
+try:
+    print('UTF8이 아닐경우 ')
+    FH = open('참가자.txt','r')  # encoding= 'UTF8'을 안 붙일경우 에러남 -- why?? 한글은 ASCII에서 지원을 안하기때문
 
-FH = open('참가자.txt','r',encoding='UTF8')  # encoding= 'UTF8'을 안 붙일경우 에러남
-#  UnicodeDecodeError: 'cp949' codec can't decode byte 0xed in position 6: illegal multibyte sequence
-a = FH.readlines()
-b =[l.strip() for l in a]
-b = random.sample(b,5)
-print(f'치킨 당첨자 : {b[:1]}')
-print(f'커피 당첨자 : {b[1:]}')
-FH.close()
+    #  UnicodeDecodeError: 'cp949' codec can't decode byte 0xed in position 6: illegal multibyte sequence
+    a = FH.readlines()
+    b =[l.strip() for l in a] # strip하는 이유 : '\n'을 제거하기위해서
+    b = random.sample(b,5)
+    print(f'치킨 당첨자 : {b[:1]}')
+    print(f'커피 당첨자 : {b[1:]}')
+    FH.close()
+
+except:
+    print('UTF8을 쓸 경우')
+    FH = open('참가자.txt', 'r',encoding='UTF8')  # encoding= 'UTF8'을 안 붙일경우 에러남 -- why?? 한글은 ASCII에서 지원을 안하기때문
+
+    #  UnicodeDecodeError: 'cp949' codec can't decode byte 0xed in position 6: illegal multibyte sequence
+    a = FH.readlines()
+    b = [l.strip() for l in a]  # strip하는 이유 : '\n'을 제거하기위해서
+    b = random.sample(b, 5)
+    print(f'치킨 당첨자 : {b[:1]}')
+    print(f'커피 당첨자 : {b[1:]}')
+    FH.close()
