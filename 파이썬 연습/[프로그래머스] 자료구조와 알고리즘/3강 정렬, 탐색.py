9d5364d2e31f0dad2517c207b95da5b43b69aa70
mingtian-chan <sniwoo98@naver.com>
2022-01-03 01:55:46 +0900

    22.01.02 프로그래머스 자료구조와 알고리즘 강의 시작, 이진 탐색 실습 문제 풀어보았음. 내일 코드 더 뜯어보면서 이해하자

--- 파이썬 연습/[프로그래머스] 자료구조와 알고리즘/3강 정렬, 탐색.py
+++ 파이썬 연습/[프로그래머스] 자료구조와 알고리즘/3강 정렬, 탐색.py
@@ -6,3 +6,20 @@
 # 선형탐색 : 길이가 길어질수록 비례해서 시간이 들음 : O(n)의 시간이 든다고 볼 수있지
 
 # 이진탐색 : 리스트의 원소가 일단 크기순으로 정리되어있어야 됨 : O(log n)
+
+def solution(L, x):
+    lower = 0
+    upper = len(L) -1
+    idx = -1
+    while lower <= upper:
+        middle = int((lower+upper)//2)
+        if x == L[middle]:
+            idx = middle
+            break
+        elif x > L[middle]:
+            lower = middle+1
+        else:
+            upper = middle-1
+
+
+print(solution([2, 3, 5, 6, 9, 11, 15],6))
